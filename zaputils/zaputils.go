e8608aa7e2ae84805133e3de359fdfdf55df02c9
Pedro-Pessoa <pedro_santos_40@hotmail.com>
2022-09-11 19:57:17 -0300

    zaputils: create docs

--- zaputils/zaputils.go
+++ zaputils/zaputils.go
@@ -14,10 +14,26 @@ const (
 	logFile = "logs.log"
 )
 
+// NewProdLogger creates a new zap logger that is production ready.
+// It consists of three "sub" loggers: a console logger, a file logger and a system logger.
+// The configs used for each of these loggers are, respectively: ConsoleConfig, FileConfig and SysConfig.
+// But, the levels of each config is overwritten as follows: the console logger will log Info level and above,
+// the file and the system loggers will log Warn level and above.
+//
+// The system logger only works on non windows systems.
 func NewProdLogger(name, facility string, priority syslog.Priority) (*zap.Logger, error) {
 	return NewCustomLogger(name, facility, priority, ConsoleConfig(), FileConfig(), SysConfig())
 }
 
+// NewCustomLogger creates a new zap logger that consists of 3 sub loggers.
+//
+// cConfig is the config intended to be used as a console logger.
+// The error sink will be grabbed from this config ErrorOutputPaths.
+//
+// fConfig is the config intended to be used as a file logger. The file path is `logs.log`.
+//
+// sConfig is the config inteded to be used as a system logger.
+// This will only work on non windows systems.
 func NewCustomLogger(name, facility string, priority syslog.Priority, cConfig, fConfig, sConfig zap.Config) (*zap.Logger, error) {
 	cEncoder := zapcore.NewConsoleEncoder(cConfig.EncoderConfig)
 	fEncoder := zapcore.NewJSONEncoder(fConfig.EncoderConfig)
@@ -77,6 +93,7 @@ func NewCustomLogger(name, facility string, priority syslog.Priority, cConfig, f
 	return zap.New(core, opts...).Named(name), nil
 }
 
+// ConsoleConfig returns a config that is intended to be used in a console logger.
 func ConsoleConfig() zap.Config {
 	cfg := zap.NewDevelopmentConfig()
 	cfg.EncoderConfig.EncodeLevel = zapcore.CapitalColorLevelEncoder
@@ -86,6 +103,7 @@ func ConsoleConfig() zap.Config {
 	return cfg
 }
 
+// FileConfig returns a config that is intended to be used in a file logger.
 func FileConfig() zap.Config {
 	cfg := zap.NewProductionConfig()
 	cfg.EncoderConfig.TimeKey = "timestamp"
@@ -95,10 +113,14 @@ func FileConfig() zap.Config {
 	return cfg
 }
 
+// SysConfig returns a config that is inteded to be used in a system logger.
+// Note that the Output path is set to an empty slice of strings, because
+// this config is meant to be used with a custom writer.
 func SysConfig() zap.Config {
 	cfg := zap.NewProductionConfig()
 	cfg.EncoderConfig.TimeKey = "timestamp"
 	cfg.EncoderConfig.EncodeTime = zapcore.ISO8601TimeEncoder
 	cfg.ErrorOutputPaths = []string{logFile, "stderr"}
+	cfg.OutputPaths = []string{}
 	return cfg
 }
