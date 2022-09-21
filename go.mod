f8c9f90fde50582268471561f9f3de6d40503808
Pedro-Pessoa <pedro_santos_40@hotmail.com>
2022-09-21 16:26:11 -0300

    zaputils: implement rotating loggers

--- go.mod
+++ go.mod
@@ -7,12 +7,15 @@ require (
 	github.com/hashicorp/go-syslog v1.0.0
 	github.com/stretchr/testify v1.8.0
 	go.uber.org/zap v1.23.0
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0
 )
 
 require (
+	github.com/BurntSushi/toml v1.2.0 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	go.uber.org/atomic v1.10.0 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- zaputils/zaputils.go
+++ zaputils/zaputils.go
@@ -8,6 +8,7 @@ import (
 	syslog "github.com/hashicorp/go-syslog"
 	"go.uber.org/zap"
 	"go.uber.org/zap/zapcore"
+	"gopkg.in/natefinch/lumberjack.v2"
 )
 
 const (
@@ -20,9 +21,33 @@ const (
 // But, the levels of each config is overwritten as follows: the console logger will log Info level and above,
 // the file and the system loggers will log Warn level and above.
 //
+// NewProdLogger does not implement a rotating logger for the file logger. If you want that functionality
+// use NewRotatingProdLogger instead.
+//
 // The system logger only works on non windows systems.
 func NewProdLogger(name, facility string, priority syslog.Priority) (*zap.Logger, error) {
 	return NewCustomLogger(name, facility, priority, ConsoleConfig(), FileConfig(), SysConfig())
+}
+
+// NewRotatingProdLogger creates a new zap logger that is production ready.
+// It consists of three "sub" loggers: a console logger, a file logger and a system logger.
+// The configs used for each of these loggers are, respectively: ConsoleConfig, FileConfig and SysConfig.
+// But, the levels of each config is overwritten as follows: the console logger will log Info level and above,
+// the file and the system loggers will log Warn level and above.
+//
+// NewRotatingProdLogger implements a rotating logger system for the file logger.
+// The max file size is 100mb, max age is 14 days, and rotated files will be compressed.
+// If a custom configuration for the rotation is needed, call NewRotatingCustomLogger instead.
+//
+// The system logger only works on non windows systems.
+func NewRotatingProdLogger(name, facility string, priority syslog.Priority) (*zap.Logger, error) {
+	lj := &lumberjack.Logger{
+		Filename: logFile,
+		MaxSize:  100,
+		MaxAge:   14,
+		Compress: true,
+	}
+	return NewRotatingCustomLogger(name, facility, priority, ConsoleConfig(), FileConfig(), SysConfig(), lj)
 }
 
 // NewCustomLogger creates a new zap logger that consists of 3 sub loggers.
@@ -31,64 +56,30 @@ func NewProdLogger(name, facility string, priority syslog.Priority) (*zap.Logger
 // The error sink will be grabbed from this config ErrorOutputPaths.
 //
 // fConfig is the config intended to be used as a file logger. The file path is `logs.log`.
+// This logger does not implement a rotating logger, if you want that functonality call
+// NewRotatingCustomLogger instead.
 //
 // sConfig is the config inteded to be used as a system logger.
 // This will only work on non windows systems.
 func NewCustomLogger(name, facility string, priority syslog.Priority, cConfig, fConfig, sConfig zap.Config) (*zap.Logger, error) {
-	cEncoder := zapcore.NewConsoleEncoder(cConfig.EncoderConfig)
-	fEncoder := zapcore.NewJSONEncoder(fConfig.EncoderConfig)
-	sEncoder := zapcore.NewJSONEncoder(sConfig.EncoderConfig)
+	return newCustomLogger(name, facility, priority, cConfig, fConfig, sConfig, nil)
+}
 
-	cSink, cCloseOut, err := zap.Open(cConfig.OutputPaths...)
-	if err != nil {
-		return nil, fmt.Errorf("zaputils: failed to open console config output paths: %w", err)
+// NewRotatingCustomLogger creates a new zap logger that consists of 3 sub loggers.
+//
+// cConfig is the config intended to be used as a console logger.
+// The error sink will be grabbed from this config ErrorOutputPaths.
+//
+// fConfig is the config intended to be used as a file logger. The file path is `logs.log`.
+// This logger will implement a rotating log system based on lj.
+//
+// sConfig is the config inteded to be used as a system logger.
+// This will only work on non windows systems.
+func NewRotatingCustomLogger(name, facility string, priority syslog.Priority, cConfig, fConfig, sConfig zap.Config, lj *lumberjack.Logger) (*zap.Logger, error) {
+	if lj == nil {
+		return nil, fmt.Errorf("zaputils: nil lumberjack logger passed")
 	}
-
-	defer func() {
-		if err != nil {
-			cCloseOut()
-		}
-	}()
-
-	errSink, _, err := zap.Open(cConfig.ErrorOutputPaths...)
-	if err != nil {
-		return nil, fmt.Errorf("zaputils: failed to open error output paths: %w", err)
-	}
-
-	fSink, fCloseOut, err := zap.Open(fConfig.OutputPaths...)
-	if err != nil {
-		return nil, fmt.Errorf("zaputils: failed to open file config output paths: %w", err)
-	}
-
-	defer func() {
-		if err != nil {
-			fCloseOut()
-		}
-	}()
-
-	cCore := zapcore.NewCore(cEncoder, cSink, zap.InfoLevel)
-	fCore := zapcore.NewCore(fEncoder, fSink, zap.WarnLevel)
-	var sCore zapcore.Core
-
-	if runtime.GOOS != "windows" {
-		var sysLogger syslog.Syslogger
-		sysLogger, err = syslog.NewLogger(priority, facility, name)
-		if err != nil {
-			return nil, fmt.Errorf("zaputils: failed to open sys config output paths: %w", err)
-		}
-
-		sWriter := zapcore.Lock(zapcore.AddSync(sysLogger))
-		sCore = zapcore.NewCore(sEncoder, sWriter, zap.WarnLevel)
-	}
-
-	cores := []zapcore.Core{cCore, fCore}
-	if sCore != nil {
-		cores = append(cores, sCore)
-	}
-
-	opts := []zap.Option{zap.ErrorOutput(errSink)}
-	core := zapcore.NewTee(cores...)
-	return zap.New(core, opts...).Named(name), nil
+	return newCustomLogger(name, facility, priority, cConfig, fConfig, sConfig, lj)
 }
 
 // ConsoleConfig returns a config that is intended to be used in a console logger.
@@ -121,4 +112,69 @@ func SysConfig() zap.Config {
 	cfg.ErrorOutputPaths = []string{logFile, "stderr"}
 	cfg.OutputPaths = []string{}
 	return cfg
+}
+
+func newCustomLogger(name, facility string, priority syslog.Priority, cConfig, fConfig, sConfig zap.Config, lj *lumberjack.Logger) (*zap.Logger, error) {
+	cEncoder := zapcore.NewConsoleEncoder(cConfig.EncoderConfig)
+	fEncoder := zapcore.NewJSONEncoder(cConfig.EncoderConfig)
+	sEncoder := zapcore.NewJSONEncoder(sConfig.EncoderConfig)
+
+	cSink, cCloseOut, err := zap.Open(cConfig.OutputPaths...)
+	if err != nil {
+		return nil, fmt.Errorf("zaputils: failed to open console config output paths: %w", err)
+	}
+
+	defer func() {
+		if err != nil {
+			cCloseOut()
+		}
+	}()
+
+	errSink, _, err := zap.Open(cConfig.ErrorOutputPaths...)
+	if err != nil {
+		return nil, fmt.Errorf("zaputils: failed to open error output paths: %w", err)
+	}
+
+	var fSink zapcore.WriteSyncer
+	if lj != nil {
+		fSink = zapcore.AddSync(lj)
+	}
+
+	if fSink == nil {
+		var fCloseOut func()
+		fSink, fCloseOut, err = zap.Open(fConfig.OutputPaths...)
+		if err != nil {
+			return nil, fmt.Errorf("zaputils: failed to open file config output paths: %w", err)
+		}
+
+		defer func() {
+			if err != nil {
+				fCloseOut()
+			}
+		}()
+	}
+
+	cCore := zapcore.NewCore(cEncoder, cSink, zap.InfoLevel)
+	fCore := zapcore.NewCore(fEncoder, fSink, zap.WarnLevel)
+	var sCore zapcore.Core
+
+	if runtime.GOOS != "windows" {
+		var sysLogger syslog.Syslogger
+		sysLogger, err = syslog.NewLogger(priority, facility, name)
+		if err != nil {
+			return nil, fmt.Errorf("zaputils: failed to open sys config output paths: %w", err)
+		}
+
+		sWriter := zapcore.Lock(zapcore.AddSync(sysLogger))
+		sCore = zapcore.NewCore(sEncoder, sWriter, zap.WarnLevel)
+	}
+
+	cores := []zapcore.Core{cCore, fCore}
+	if sCore != nil {
+		cores = append(cores, sCore)
+	}
+
+	opts := []zap.Option{zap.ErrorOutput(errSink)}
+	core := zapcore.NewTee(cores...)
+	return zap.New(core, opts...).Named(name), nil
 }
