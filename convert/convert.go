10aedf43d9dbd33ee549a774632e6d74a7f75183
Pedro-Pessoa <pedro_santos_40@hotmail.com>
2022-11-21 18:09:18 -0300

    convert: ToInt64 now returns an error

--- convert/convert.go
+++ convert/convert.go
@@ -1,54 +1,54 @@
 package convert
 
 import (
+	"fmt"
 	"strconv"
 	"time"
 	"unsafe"
 )
 
-// ToInt converts from to an int64.
-func ToInt64(from any) int64 {
+// ToInt64 converts from to an int64.
+func ToInt64(from any) (int64, error) {
 	switch t := from.(type) {
 	case int:
-		return int64(t)
+		return int64(t), nil
 	case int8:
-		return int64(t)
+		return int64(t), nil
 	case int16:
-		return int64(t)
+		return int64(t), nil
 	case int32:
-		return int64(t)
+		return int64(t), nil
 	case int64:
-		return t
+		return t, nil
 	case float32:
-		return int64(t)
+		return int64(t), nil
 	case float64:
-		return int64(t)
+		return int64(t), nil
 	case uint:
-		return int64(t)
+		return int64(t), nil
 	case uint8:
-		return int64(t)
+		return int64(t), nil
 	case uint16:
-		return int64(t)
+		return int64(t), nil
 	case uint32:
-		return int64(t)
+		return int64(t), nil
 	case uint64:
-		return int64(t)
+		return int64(t), nil
 	case string:
-		parsed, _ := strconv.ParseInt(t, 10, 64)
-		return parsed
+		return strconv.ParseInt(t, 10, 64)
 	case time.Duration:
-		return int64(t)
+		return int64(t), nil
 	case time.Month:
-		return int64(t)
+		return int64(t), nil
 	case time.Weekday:
-		return int64(t)
+		return int64(t), nil
 	case bool:
 		if t {
-			return 1
+			return 1, nil
 		}
-		return 0
+		return 0, nil
 	default:
-		return 0
+		return 0, fmt.Errorf("convert: can not convert type %T to int64", t)
 	}
 }
 
--- convert/convert_test.go
+++ convert/convert_test.go
@@ -9,33 +9,37 @@ import (
 
 func TestToInt64(t *testing.T) {
 	for _, tc := range []struct {
-		name  string
-		input any
-		want  int64
+		name    string
+		input   any
+		want    int64
+		wantErr bool
 	}{
-		{"int", int(10), 10},
-		{"int8", int8(10), 10},
-		{"int16", int16(10), 10},
-		{"int32", int32(10), 10},
-		{"int64", int64(10), 10},
-		{"float32", float32(10), 10},
-		{"float64", float64(10), 10},
-		{"uint", uint(10), 10},
-		{"uint8", uint8(10), 10},
-		{"uint16", uint16(10), 10},
-		{"uint32", uint32(10), 10},
-		{"uint64", uint64(10), 10},
-		{"string-10", "10", 10},
-		{"string-a", "a", 0},
-		{"time-nanosecond", time.Nanosecond, 1},
-		{"time-month-5", time.Month(5), 5},
-		{"time-weekday-5", time.Weekday(5), 5},
-		{"true", true, 1},
-		{"false", false, 0},
-		{"empty-[]byte", []byte{}, 0},
+		{"int", int(10), 10, false},
+		{"int8", int8(10), 10, false},
+		{"int16", int16(10), 10, false},
+		{"int32", int32(10), 10, false},
+		{"int64", int64(10), 10, false},
+		{"float32", float32(10), 10, false},
+		{"float64", float64(10), 10, false},
+		{"uint", uint(10), 10, false},
+		{"uint8", uint8(10), 10, false},
+		{"uint16", uint16(10), 10, false},
+		{"uint32", uint32(10), 10, false},
+		{"uint64", uint64(10), 10, false},
+		{"string-10", "10", 10, false},
+		{"string-a", "a", 0, true},
+		{"time-nanosecond", time.Nanosecond, 1, false},
+		{"time-month-5", time.Month(5), 5, false},
+		{"time-weekday-5", time.Weekday(5), 5, false},
+		{"true", true, 1, false},
+		{"false", false, 0, false},
+		{"empty-[]byte", []byte{}, 0, true},
 	} {
 		t.Run(tc.name, func(t *testing.T) {
-			assert.Equal(t, tc.want, ToInt64(tc.input))
+			got, err := ToInt64(tc.input)
+			if ((err != nil) != tc.wantErr) || tc.want != got {
+				t.Errorf("\ntest '%s' failed to convert\nwant: %v\ngot: %v\nwantErr: %v\nerr: %v", tc.name, tc.want, got, tc.wantErr, err)
+			}
 		})
 	}
 }
