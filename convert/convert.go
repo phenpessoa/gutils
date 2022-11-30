34cb15447c61d1048598bad115ce0745f9c1733b
Pedro-Pessoa <pedro_santos_40@hotmail.com>
2022-11-30 18:55:36 -0300

    convert: refactor the convert package

--- convert/convert.go
+++ convert/convert.go
@@ -6,47 +6,271 @@ import (
 	"time"
 )
 
+// ToInt converts from to an int.
+//
+// If from is an integer or a float, a time.Duration,
+// a time.Month or a time.Weekday a direct
+// type convertion will be made.
+//
+// If from is a bool, it will return 1 for
+// true and 0 for false.
+//
+// If from is time.Time, it will return
+// the Unix time.
+//
+// If it is a string, strconv will be used.
+//
+// No other types are allowed.
+func ToInt(from any) (int, error) {
+	return toInteger[int](from, false)
+}
+
+// ToInt8 converts from to an int8.
+//
+// If from is an integer or a float, a time.Duration,
+// a time.Month or a time.Weekday a direct
+// type convertion will be made.
+//
+// If from is a bool, it will return 1 for
+// true and 0 for false.
+//
+// If from is time.Time, it will return
+// the Unix time.
+//
+// If it is a string, strconv will be used.
+//
+// No other types are allowed.
+func ToInt8(from any) (int8, error) {
+	return toInteger[int8](from, false)
+}
+
+// ToInt16 converts from to an int16.
+//
+// If from is an integer or a float, a time.Duration,
+// a time.Month or a time.Weekday a direct
+// type convertion will be made.
+//
+// If from is a bool, it will return 1 for
+// true and 0 for false.
+//
+// If from is time.Time, it will return
+// the Unix time.
+//
+// If it is a string, strconv will be used.
+//
+// No other types are allowed.
+func ToInt16(from any) (int16, error) {
+	return toInteger[int16](from, false)
+}
+
+// ToInt32 converts from to an int32.
+//
+// If from is an integer or a float, a time.Duration,
+// a time.Month or a time.Weekday a direct
+// type convertion will be made.
+//
+// If from is a bool, it will return 1 for
+// true and 0 for false.
+//
+// If from is time.Time, it will return
+// the Unix time.
+//
+// If it is a string, strconv will be used.
+//
+// No other types are allowed.
+func ToInt32(from any) (int32, error) {
+	return toInteger[int32](from, false)
+}
+
 // ToInt64 converts from to an int64.
+//
+// If from is an integer or a float, a time.Duration,
+// a time.Month or a time.Weekday a direct
+// type convertion will be made.
+//
+// If from is a bool, it will return 1 for
+// true and 0 for false.
+//
+// If from is time.Time, it will return
+// the Unix time.
+//
+// If it is a string, strconv will be used.
+//
+// No other types are allowed.
 func ToInt64(from any) (int64, error) {
+	return toInteger[int64](from, false)
+}
+
+// ToUint converts from to an uint.
+//
+// If from is an integer or a float, a time.Duration,
+// a time.Month or a time.Weekday a direct
+// type convertion will be made.
+//
+// If from is a bool, it will return 1 for
+// true and 0 for false.
+//
+// If from is time.Time, it will return
+// the Unix time.
+//
+// If it is a string, strconv will be used.
+//
+// No other types are allowed.
+func ToUint(from any) (uint, error) {
+	return toInteger[uint](from, true)
+}
+
+// ToUint8 converts from to an uint8.
+//
+// If from is an integer or a float, a time.Duration,
+// a time.Month or a time.Weekday a direct
+// type convertion will be made.
+//
+// If from is a bool, it will return 1 for
+// true and 0 for false.
+//
+// If from is time.Time, it will return
+// the Unix time.
+//
+// If it is a string, strconv will be used.
+//
+// No other types are allowed.
+func ToUint8(from any) (uint8, error) {
+	return toInteger[uint8](from, true)
+}
+
+// ToUint16 converts from to an uint16.
+//
+// If from is an integer or a float, a time.Duration,
+// a time.Month or a time.Weekday a direct
+// type convertion will be made.
+//
+// If from is a bool, it will return 1 for
+// true and 0 for false.
+//
+// If from is time.Time, it will return
+// the Unix time.
+//
+// If it is a string, strconv will be used.
+//
+// No other types are allowed.
+func ToUint16(from any) (uint16, error) {
+	return toInteger[uint16](from, true)
+}
+
+// ToUint32 converts from to an uint32.
+//
+// If from is an integer or a float, a time.Duration,
+// a time.Month or a time.Weekday a direct
+// type convertion will be made.
+//
+// If from is a bool, it will return 1 for
+// true and 0 for false.
+//
+// If from is time.Time, it will return
+// the Unix time.
+//
+// If it is a string, strconv will be used.
+//
+// No other types are allowed.
+func ToUint32(from any) (uint32, error) {
+	return toInteger[uint32](from, true)
+}
+
+// ToUint64 converts from to an uint64.
+//
+// If from is an integer or a float, a time.Duration,
+// a time.Month or a time.Weekday a direct
+// type convertion will be made.
+//
+// If from is a bool, it will return 1 for
+// true and 0 for false.
+//
+// If from is time.Time, it will return
+// the Unix time.
+//
+// If it is a string, strconv will be used.
+//
+// No other types are allowed.
+func ToUint64(from any) (uint64, error) {
+	return toInteger[uint64](from, true)
+}
+
+// ToUintptr converts from to an uintptr.
+//
+// If from is an integer or a float, a time.Duration,
+// a time.Month or a time.Weekday a direct
+// type convertion will be made.
+//
+// If from is a bool, it will return 1 for
+// true and 0 for false.
+//
+// If from is time.Time, it will return
+// the Unix time.
+//
+// If it is a string, strconv will be used.
+//
+// No other types are allowed.
+func ToUintptr(from any) (uintptr, error) {
+	return toInteger[uintptr](from, true)
+}
+
+func toInteger[To strictInteger](from any, unsigned bool) (To, error) {
 	switch t := from.(type) {
 	case int:
-		return int64(t), nil
+		return To(t), nil
 	case int8:
-		return int64(t), nil
+		return To(t), nil
 	case int16:
-		return int64(t), nil
+		return To(t), nil
 	case int32:
-		return int64(t), nil
+		return To(t), nil
 	case int64:
-		return t, nil
+		return To(t), nil
 	case float32:
-		return int64(t), nil
+		return To(t), nil
 	case float64:
-		return int64(t), nil
+		return To(t), nil
 	case uint:
-		return int64(t), nil
+		return To(t), nil
 	case uint8:
-		return int64(t), nil
+		return To(t), nil
 	case uint16:
-		return int64(t), nil
+		return To(t), nil
 	case uint32:
-		return int64(t), nil
+		return To(t), nil
 	case uint64:
-		return int64(t), nil
-	case string:
-		return strconv.ParseInt(t, 10, 64)
+		return To(t), nil
+	case uintptr:
+		return To(t), nil
 	case time.Duration:
-		return int64(t), nil
+		return To(t), nil
 	case time.Month:
-		return int64(t), nil
+		return To(t), nil
 	case time.Weekday:
-		return int64(t), nil
+		return To(t), nil
+	case time.Time:
+		return To(t.Unix()), nil
 	case bool:
 		if t {
 			return 1, nil
 		}
 		return 0, nil
+	case string:
+		if unsigned {
+			x, err := strconv.ParseUint(t, 10, 64)
+			return To(x), err
+
+		}
+		x, err := strconv.ParseInt(t, 10, 64)
+		return To(x), err
 	default:
-		return 0, fmt.Errorf("convert: can not convert type %T to int64", t)
+		var zero To
+		return 0, fmt.Errorf("can not convert type '%T' to '%T'", t, zero)
 	}
+}
+
+type strictInteger interface {
+	int | int8 | int16 | int32 | int64 |
+		uint | uint8 | uint16 | uint32 | uint64 | uintptr
 }
